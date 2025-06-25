bec1cbe369489f6c20d36d19e3256d602cc2cdc0
Bex031 <dopple31@gmail.com>
2025-06-25 13:50:38 +0900

    c7 style fix

--- fixFlavor/iho/html_convert.rb
+++ fixFlavor/iho/html_convert.rb
@@ -1,13 +1,20 @@
 require "isodoc"
+require "isodoc/html_convert"   # 🔥 이거 반드시 추가!
 require "isodoc/generic/html_convert"
 require_relative "init"
 
 module IsoDoc
   module Iho
     class HtmlConvert < IsoDoc::Generic::HtmlConvert
-
       include BaseConvert
       include Init
+
+      # 🔥 기존 generic 설정 무시하고 scripts.html만 로딩
+      def default_file_locations(_options)
+ 	 super.merge(
+   	 scripts: File.join(File.dirname(__FILE__), "html", "scripts.html")
+ 	 )
+	end
     end
   end
 end
